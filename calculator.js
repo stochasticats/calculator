04728bcbb755d770a5ab792b59821184517aa952
Jamie <nordio.jamie@gmail.com>
2022-10-12 15:11:14 -0700

    reorganize code structure and add comments

--- calculator.js
+++ calculator.js
@@ -1,22 +1,58 @@
-// add a, b
+// initialize calculator variables
+let display = document.getElementById("display");
+display.textContent = "";
+let previousValue = 0;
+let operation;
+
+// set up digit click events
+const digits = document.querySelectorAll(".digit");
+digits.forEach((digit) => {
+    digit.addEventListener("click", (event) => {
+        display.textContent += digit.id;
+    });
+});
+
+// clears display and variables
+const clear = document.getElementById("clear");
+clear.addEventListener("click", (event) => {
+    display.textContent = "";
+    operation = "";
+    previousValue = 0;
+});
+
+// set up operator events
+const operators = document.querySelectorAll(".operator")
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        operation = operator.id;
+        previousValue = Number(display.textContent);
+        display.textContent = "";
+    });
+});
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", (event) => {
+    const toDisplay = operate(operation, previousValue, Number(display.textContent));
+    display.textContent = toDisplay;
+    previousValue = toDisplay;
+});
+
+// Functions for calculator functionality
 function add(a, b) {
     return a+b;
 }
 
-// subtract b from a
 function subtract(a, b) {
     return a-b;
 }
 
-// multiply a by b
 function multiply(a, b) {
     return a*b;
 }
 
-// divide a by b
 function divide(a, b) {
     if (b === 0) {
-        return "ERROR: cannot divide by 0";
+        return "Cannot divide by 0.";
     } else {
         return a/b;
     }
@@ -26,7 +62,6 @@ function power(base, power) {
     return base**power;
 }
 
-// pass in add, subtract, multiply, divide, or power and return desired result
 function operate(operation, a, b) {
     switch (operation) {
         case "add":
@@ -43,38 +78,3 @@ function operate(operation, a, b) {
             break;
     }
 }
-
-let display = document.getElementById("display");
-display.textContent = "";
-let previousValue = 0;
-let operation;
-
-const digits = document.querySelectorAll(".digit");
-digits.forEach((digit) => {
-    digit.addEventListener("click", (event) => {
-        display.textContent += digit.id;
-    });
-});
-
-const clear = document.getElementById("clear");
-clear.addEventListener("click", (event) => {
-    display.textContent = "";
-    operation = "";
-    previousValue = 0;
-});
-
-const operators = document.querySelectorAll(".operator")
-operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        operation = operator.id;
-        previousValue = Number(display.textContent);
-        display.textContent = "";
-    });
-});
-
-const equals = document.getElementById("equals");
-equals.addEventListener("click", (event) => {
-    const toDisplay = operate(operation, previousValue, Number(display.textContent));
-    display.textContent = toDisplay;
-    previousValue = toDisplay;
-});
